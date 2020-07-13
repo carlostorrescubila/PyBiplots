af4b9a0f259ccf0ad10b05172052e5c49910ea3e
Carlos Alfredo Torres Cubilla <44613865+carlostorrescubila@users.noreply.github.com>
2020-07-13 11:06:27 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='PyBiplots',
+      version='0.1.0',
+      author='Carlos A. Torres Cubilla',
+      author_email='carlos_t22@usal.es',
+      description='Package to performance some biplots methods',
+      url='https://github.com/carlostorrescubila',   
+      license='MIT',
+      #packages=['pybiplots'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'scipy',
+          'matplotlib',
+          'seaborn',
+          'sklearn',
+          'adjustText'
+      ],
+      packages=find_packages(),
+      package_dir={'pybiplots': 'pybiplots'}, 
+      zip_safe=False
+     )
