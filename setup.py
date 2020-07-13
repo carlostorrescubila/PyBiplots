6727a12ce5420c5ee28aa2e0686f797009501338
Carlos Alfredo Torres Cubilla <44613865+carlostorrescubila@users.noreply.github.com>
2020-07-13 12:22:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
-from setuptools import find_packages
+#from setuptools import find_packages
 
-setup(name='PyBiplots',
+setup(name='pybiplots',
       version='0.1.0',
       author='Carlos A. Torres Cubilla',
       author_email='carlos_t22@usal.es',
       description='Package to performance some biplots methods',
       url='https://github.com/carlostorrescubila',   
       license='MIT',
-      #packages=['pybiplots'],
+      packages=['pybiplots'],
       install_requires=[
           'numpy',
           'pandas',
@@ -18,7 +18,7 @@ setup(name='PyBiplots',
           'sklearn',
           'adjustText'
       ],
-      packages=find_packages(),
-      package_dir={'pybiplots': 'pybiplots'}, 
+      #packages=find_packages(),
+      #package_dir={'pybiplots': 'pybiplots'}, 
       zip_safe=False
      )
