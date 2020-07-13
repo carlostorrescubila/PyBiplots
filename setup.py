cdef288abc32ee44fd94037994b8f1285ee3f6e1
Carlos Alfredo Torres Cubilla <44613865+carlostorrescubila@users.noreply.github.com>
2020-07-14 01:47:11 +0200

    Add files via upload
    Version 0.2.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='pybiplots',
-      version='0.2.0',
+setup(name='PyBiplots',
+      version='0.2.1',
       author='Carlos A. Torres Cubilla',
       author_email='carlos_t22@usal.es',
       description='Package to performance some biplots methods',
